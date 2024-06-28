f5ad608713175c640eb24b01fa1007760772016c
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2024-06-28 01:03:26 +0000

    chore: Refactor rhythm animation logic and add sound effects

--- game/app/page.tsx
+++ game/app/page.tsx
@@ -21,55 +21,81 @@ export default function Home() {
     { dur: 8, rest: false, compl: false },
     { dur: 2, rest: false, compl: false },
     { dur: 2, rest: false, compl: false },
+    { dur: 16, rest: false, compl: false },
+    { dur: 16, rest: false, compl: false },
+    { dur: 16, rest: false, compl: false },
+    { dur: 16, rest: false, compl: false },
+    { dur: 1, rest: true, compl: false },
   ];
   rhythms.forEach((r, i) => (r.index = i));
-  let bpm = 120;
+  let [stateRhythms, setStateRhythms] = useState(rhythms);
+  let bpm = 60;
   let frames = [];
   rhythms.forEach((rhythm) => {
+    let indicator = "out-of-range";
+
     for (let i = 0; i < 800 / rhythm.dur / (bpm / 60); i++) {
-      frames.push([rhythm.index, i < 20 ? 1 : 0]);
+      indicator = "out-of-range";
+      if (i < (800 * 0.2) / rhythm.dur / (bpm / 60)) indicator = "in-range";
+      if (i >= (800 * 0.8) / rhythm.dur / (bpm / 60)) indicator = "early";
+
+      frames.push([rhythm.index, indicator]);
     }
   });
-  console.log(frames);
-
   const barRef = useRef(null);
   const line = useRef(null);
   let [started, setStarted] = useState(false);
+  function playClick() {
+    let audio = new Audio("/click.mp3");
+    audio.play();
+  }
+  function playSnare() {
+    let audio = new Audio("/snare.mp3");
+    audio.play();
+  }
   function start() {
     let frame = 0;
     setStarted(true);
     setInterval(() => {
       barRef.current.style.transform += `translateX(-${bpm / 60}px)`;
+      if (frame % (200 / (bpm / 60)) == 0 && frame < frames.length) playClick();
+
       frame++;
     }, 5);
-    window.addEventListener("click", (e) => {
-      if (frames[frame][1] == 1) {
-        rhythms[frames[frame][0]].rest = true;
+    function handleTap() {
+      if (frame >= frames.length) return;
+      playSnare();
+      if (stateRhythms[frames[frame][0]].rest) return;
+      if (frames[frame][1] == "in-range") {
+        console.log("compl");
+        setStateRhythms((rhythms) => {
+          let newRhythms = [...rhythms];
+          newRhythms[frames[frame][0]].compl = true;
+          return newRhythms;
+        });
         frame++;
+      } else if (frames[frame][1] == "early") {
+        if (!frames[frame + 1]) return;
+        console.log("early");
+        setStateRhythms((rhythms) => {
+          let newRhythms = [...rhythms];
+          newRhythms[frames[frame + 1][0]].compl = true;
+          return newRhythms;
+        });
+      } else {
+        console.log("fail");
+        setStateRhythms((rhythms) => {
+          let newRhythms = [...rhythms];
+          newRhythms[frames[frame][0]].fail = true;
+          return newRhythms;
+        });
       }
-      // let element = findElementAtPosition(
-      //   line.current.getBoundingClientRect().x,
-      //   line.current.getBoundingClientRect().y
-      // );
-      // element?.classList.add("bg-red-500");
+    }
+    window.addEventListener("keydown", (e) => {
+      handleTap();
     });
   }
 
-  // function findElementAtPosition(x, y) {
-  //   const elements = [];
-  //   let element = document.elementFromPoint(x, y);
-
-  //   while (element && element !== document.documentElement) {
-  //     elements.push(element);
-  //     element.style.pointerEvents = "none"; // Temporarily hide the element
-  //     element = document.elementFromPoint(x, y);
-  //   }
-
-  //   // Restore pointer events
-  //   elements.forEach((el) => (el.style.pointerEvents = ""));
-
-  //   return elements.find((e) => e.classList.contains("bg-sky-600"));
-  // }
   return (
     <main>
       <h1 className="text-6xl text-center m-8 font-bold text-sky-600">
@@ -77,15 +103,19 @@ export default function Home() {
       </h1>
       <div className=" m-8 overflow-hidden flex">
         <span className="bg-sky-700 w-4 z-20" ref={line}></span>
-        <div className="flex flex-nowrap" ref={barRef}>
-          {rhythms.map((rhythm, index) => (
+        <div className="flex flex-nowrap ml-4" ref={barRef}>
+          {stateRhythms.map((rhythm, index) => (
             <div
               key={index}
               className={`relative  text-white ${
-                !rhythm.rest ? "bg-sky-600" : "bg-gray-400"
-              } ${
-                rhythm.compl && "bg-green-600"
-              } flex items-center justify-center p-4`}
+                rhythm.compl
+                  ? "bg-green-600"
+                  : rhythm.fail
+                  ? "bg-red-600"
+                  : rhythm.rest
+                  ? "bg-gray-500"
+                  : "bg-sky-600"
+              } flex items-center justify-center p-4 `}
               style={{
                 flex: `${800 / rhythm.dur - 10}px 0 0`,
                 height: "200px",
